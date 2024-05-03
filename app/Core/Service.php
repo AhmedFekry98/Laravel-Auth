655460c49e4a37231c66828d4a11e6bc1289e31b
abdulrhman-developer0 <tknwtknwlwjy10@gmail.com>
2024-05-03 18:37:08 +0300

    add stop method to abstract sService

--- app/Core/Service.php
+++ app/Core/Service.php
@@ -3,7 +3,9 @@
 namespace App\Core;
 
 use App\TDO\ServiceTDO;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Modules\Auth\Enums\ErrorCode;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 abstract class Service
 {
@@ -15,5 +17,12 @@ abstract class Service
     protected function success(mixed $daata): ServiceTDO
     {
         return new ServiceTDO($daata);
+    }
+
+    public function stop(mixed $daata): void
+    {
+        throw new HttpResponseException(
+            response: response()->json($daata)
+        );
     }
 }
